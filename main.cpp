51c1876267eeee823ac508b0d2ca6d55501b73fa
Malakye Brown <prophetoftruth07@gmail.com>
2026-01-13 10:48:01 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// IDE_and_VCS_Workflow_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+
+}
